590d16495f6029ff218c49c95a8d3ca8dcd49b34
unknown <cami.rivas.dlb@gmail.com>
2025-09-24 20:16:17 -0300

    User Model

---
+++ Backend/src/models/User.model.js
@@ -0,0 +1,40 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: [true, 'El nombre es requerido'],
+    trim: true,
+    unique: true,
+    maxlength: [50, 'El nombre no puede exceder 50 caracteres']
+  },
+  email: {
+    type: String,
+    required: [true, 'El email es requerido'],
+    unique: true,
+    lowercase: true,
+    match: [
+      /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/,
+      'Por favor ingresa un email válido'
+    ]
+  },
+  password: {
+    type: String,
+    required: [true, 'La contraseña es requerida'],
+    minlength: [8, 'La contraseña debe tener al menos 6 caracteres']
+  },
+  role: {
+    type: String,
+    enum: ['user', 'admin', 'superuser'],
+    default: 'user'
+  },
+  isActive: {
+    type: Boolean,
+    default: true
+  }
+}, {
+  timestamps: true
+});
+
+
+export default mongoose.model('User', userSchema);
