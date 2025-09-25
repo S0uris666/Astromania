adab9584fff14a79f6cf03ae0cdab6662e6249fd
unknown <cami.rivas.dlb@gmail.com>
2025-09-25 15:07:08 -0300

    cookies

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,16 +1,19 @@
 import express from "express";
 import cors from "cors";
+import cookieParser from "cookie-parser";
 const app = express();
 
 
+
 //controllers
-import authRouter from "./routes/auth.routes.js";
+import userRouter from "./routes/user.routes.js";
 import contactRouter from './routes/contact.route.js';
 import paymentRouter from "./routes/payment.route.js";
 
 // Middlewares
 app.use(cors());
 app.use(express.json());
+app.use(cookieParser());
 
 
 // Rutas
@@ -20,7 +23,7 @@ app.get("/", (req, res) => {
 
 app.use('/api', contactRouter); 
 app.use("/api/payments", paymentRouter);
-app.use("/api", authRouter);
+app.use("/api", userRouter);
 
 
 export default app;
