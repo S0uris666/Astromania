80883982f26f017bd44e9e572aec1a345c3cc68a
unknown <cami.rivas.dlb@gmail.com>
2025-09-26 12:08:59 -0300

    estructura controllers eventos

---
+++ Backend/src/controllers/event.controller.js
@@ -0,0 +1,19 @@
+import Event from '../models/Event.model.js'
+
+const getAllEvents = async (req, res) => {}
+
+const createEvent = async (req, res) => {}
+
+const updateEvent = async (req, res) => {}
+
+const deleteEvent = async (req, res) => {}
+
+const getEventById = async (req, res) => {}
+
+
+
+// Rutas públicas (sin auth, cualquiera puede usarlas)
+
+const getAllPublicEvents = async (req, res) => {}
+
+const getPublicEventById = async (req, res) => {}
