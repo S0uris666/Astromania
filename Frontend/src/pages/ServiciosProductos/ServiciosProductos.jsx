117918ebb29c4b8c8d8e167e11633afa6c536350
unknown <cami.rivas.dlb@gmail.com>
2025-09-27 18:48:45 -0300

    install axios

--- Frontend/src/pages/ServiciosProductos/ServiciosProductos.jsx
+++
@@ -1,9 +0,0 @@
-import { ServiceProductList } from "../../components/servicesProducts/list"
-
-export const ServiciosProductos = () => {
-  return (
-    <section>
-      <ServiceProductList />
-    </section>
-  )
-}
