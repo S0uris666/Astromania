5f1e643427a3545fd86143bd849eaff9e4bd4b4d
unknown <cami.rivas.dlb@gmail.com>
2025-09-27 18:38:51 -0300

    se agrega reducer y state para obtener productos y servicios

--- Frontend/src/context/serviceProducts/ServiceProductReducer.js
+++ Frontend/src/context/serviceProducts/ServiceProductReducer.js
@@ -1,5 +1,11 @@
 const ServiceProductReducer = (globalState, action) => {
     switch (action.type) {
+        case "GET_SERVICE_PRODUCTS":
+            return {
+                ...globalState,
+                serviceProduct: action.payload,
+            };
+        // case "ADD_PRODUCT":
         default:
             return globalState;
     }
--- Frontend/src/context/serviceProducts/ServiceProductState.jsx
+++ Frontend/src/context/serviceProducts/ServiceProductState.jsx
@@ -1,34 +1,38 @@
 import ServiceProductContext from "./ServiceProductContext";
-import { useReducer } from "react";
+import { useReducer, useCallback } from "react";
 import ServiceProductReducer from "./ServiceProductReducer";
+import { getServiceProducts } from "../../api/auth";
+
 
 //endporint de la api aqui
 const ServiceProductState = (props) => {
   const initialState = {
-    serviceProduct: [
-      {
-        id: 1,
-        name: "Observación Astronómica",
-        description:
-          "Sesiones guiadas para observar el cielo nocturno con telescopios y binoculares.",
-        price: 20,
-        
-        },
-      {
-        id: 2,
-        name: "Charlas y Talleres",
-        description:
-          "Presentaciones educativas sobre astronomía, astrofísica y ciencias espaciales.",
-        price: 15,
-      
-        },
-    ],
+    serviceProduct: [ ],
   };
+
 
   const[globalState, dispatch]=useReducer(ServiceProductReducer,initialState) //dispatch genera la accion Crud
 
-  return (<ServiceProductContext.Provider          /* //todo lo que este en value se podra usar en cualquier componente hijo */
-    value={{serviceProduct:globalState.serviceProduct}}>    
+
+//aqui va las funciones que gestionan el crud
+
+ const getSP = useCallback(async () => {
+    try {
+      const res = await getServiceProducts();
+      console.log("colección de productos y servicios", res);
+      dispatch({ type: "GET_PRODUCTS", payload: res.data });
+    } catch (error) {
+      console.error("Error obteniendo productos y servicios", error);
+    }
+  }, []); // dispatch es estable, no hace falta incluirlo
+
+
+
+  return (<ServiceProductContext.Provider    /* //todo lo que este en value se podra usar en cualquier componente hijo */
+                                                 
+    value={{serviceProduct:globalState.serviceProduct,
+    getSP
+    }}>    
                                               
     {props.children}
 
