634234b6018736fe6ab24eeb139cbd35ae4ead7a
unknown <cami.rivas.dlb@gmail.com>
2025-09-27 18:36:20 -0300

    se agrega getServiceProduct

---
+++ Frontend/src/api/auth.js
@@ -0,0 +1,14 @@
+import axios from "axios";
+const API = import.meta.env.VITE_BACKEND_URL;
+
+
+export const registerRequest = (user) => axios.post(`${API}/register`,user)
+export const loginRequest = (user) => axios.post(`${API}/login`,user)
+
+
+
+
+
+//Productos y servicios
+
+export const getServiceProducts= (products) => axios.get(`${API}/service-products/readall`,products)
