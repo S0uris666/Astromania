c6a5b556181f0468e0727d209ed50a2207648a51
unknown <cami.rivas.dlb@gmail.com>
2025-10-07 19:02:39 -0300

    arreglos de la url, se soluciona la key repetida

--- Frontend/src/data/Activities.jsx
+++ Frontend/src/data/Activities.jsx
@@ -1,43 +1,49 @@
 export const Activities = [
   {
     id: "reserva",
+    linkTo:"contacto",
     title: "Visitas a colegios",
     cta: "Revisa disponibilidad",
     image: "/public/Images/planetario.webp",
     description: "Llevamos la astronomía a tu colegio con actividades interactivas."
   },
    {
-    id: "servicios-productos-list",
+    id: "juegos-mesa",
     title: "Juegos de mesa",
+    linkTo:"servicios-productos-list",
     cta: "Descubre nuestros juegos",
     image: "/public/Images/juegosdemesa.webp",
     description: "Juegos originales de la fundación para aprender jugando."
   },
   
   {
-    id: "servicios-productos-list",
+    id: "luna-marte",
     title: "Exposición Misión Luna-Marte",
+    linkTo:"servicios-productos-list",
     cta: "Explora la exposición",
     image: "/public/Images/misionlunamarte.webp",
     description: "Conoce la historia y los secretos de la Luna y Marte."
   },
   {
-    id: "servicios-productos-list",
+    id: "concursos",
     title: "Concursos",
+    linkTo:"servicios-productos-list",
     cta: "Participa aquí",
     image: "/public/Images/concursos.webp",
     description: "Participa en nuestros concursos astronómicos y gana premios."
   },
   {
-    id: "servicios-productos-list",
+    id: "cuentos",
     title: "Cuentos Astronómicos",
+    linkTo:"servicios-productos-list",
     cta: "Descubre Cosmocuentos",
     image: "/public/Images/cuentos.webp",
     description: "Aprende astronomía de manera divertida con cuentos"
   },
   {
-    id: "servicios-productos-list",
+    id: "charlas-talleres",
     title: "Charlas y Talleres",
+    linkTo:"servicios-productos-list",
     cta: "Solicita información",
     image: "/public/Images/charlasytalleres.webp",
     description: "Llevamos charlas y talleres a tu institucion."
