5e3ce4a064239f5cd8d5ff47e13374f4e7647873
unknown <cami.rivas.dlb@gmail.com>
2025-10-07 18:25:39 -0300

    se agrega nuevos campos

--- Backend/src/models/User.model.js
+++ Backend/src/models/User.model.js
@@ -1,40 +1,48 @@
 import mongoose from "mongoose";
 
-const userSchema = new mongoose.Schema({
-  username: {
-    type: String,
-    required: [true, 'El nombre es requerido'],
-    trim: true,
-    unique: true,
-    maxlength: [50, 'El nombre no puede exceder 50 caracteres']
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: [true, "El nombre es requerido"],
+      trim: true,
+      unique: true,
+      maxlength: [50, "El nombre no puede exceder 50 caracteres"],
+    },
+    email: {
+      type: String,
+      required: [true, "El email es requerido"],
+      unique: true,
+      lowercase: true,
+      match: [
+        /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/,
+        "Por favor ingresa un email válido",
+      ],
+    },
+    password: {
+      type: String,
+      required: [true, "La contraseña es requerida"],
+      minlength: [8, "La contraseña debe tener al menos 6 caracteres"],
+    },
+    role: {
+      type: String,
+      enum: ["user", "admin", "superuser"],
+      default: "user",
+    },
+    address: { type: String, trim: true, default: "" },
+    city: { type: String, trim: true, default: "" }, //
+    country: { type: String, trim: true, default: "" },
+    zipcode: { type: String, trim: true, default: "" },
+    phone: { type: String, trim: true, default: "" },
+
+    isActive: {
+      type: Boolean,
+      default: true,
+    },
   },
-  email: {
-    type: String,
-    required: [true, 'El email es requerido'],
-    unique: true,
-    lowercase: true,
-    match: [
-      /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/,
-      'Por favor ingresa un email válido'
-    ]
-  },
-  password: {
-    type: String,
-    required: [true, 'La contraseña es requerida'],
-    minlength: [8, 'La contraseña debe tener al menos 6 caracteres']
-  },
-  role: {
-    type: String,
-    enum: ['user', 'admin', 'superuser'],
-    default: 'user'
-  },
-  isActive: {
-    type: Boolean,
-    default: true
+  {
+    timestamps: true,
   }
-}, {
-  timestamps: true
-});
+);
 
-
-export default mongoose.model('User', userSchema);
+export default mongoose.model("User", userSchema);
