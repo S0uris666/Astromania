241d912d3851c372b9549e7876a8d205c8907b09
unknown <cami.rivas.dlb@gmail.com>
2025-10-07 18:27:18 -0300

    fix payment route

--- Backend/src/routes/payment.route.js
+++ Backend/src/routes/payment.route.js
@@ -1,6 +1,8 @@
 import express from "express";
-import { createPreference } from "../controllers/payment.controller.js";
-import { testPreference } from "../controllers/payment.controller.js";
+import { createPreference, testPreference, getStatus,successReturn,failureReturn,
+  pendingReturn, webhook
+ } from "../controllers/payment.controller.js";
+
 
 const paymentRouter = express.Router();
 
@@ -9,14 +11,18 @@ paymentRouter.post("/create_preference", createPreference);//http://localhost:30
 paymentRouter.get("/test_preference", testPreference);//http://localhost:3000/api/payments/test_preference
 
 
-paymentRouter.get("/success", (req, res) => { //http://localhost:3000/api/payments/success
-  res.send("Ruta de pagos funcionando!");
-});
+paymentRouter.get("/status/:paymentId", getStatus); //http://localhost:3000/api/payments/status/:paymentId
+paymentRouter.get("/success", successReturn);
+paymentRouter.get("/failure", failureReturn);
+paymentRouter.get("/pending", pendingReturn);
 
 
-paymentRouter.post("/notification", (req, res) => {
+/* paymentRouter.post("/notification", (req, res) => {
   console.log("Notificación recibida:", req.body);
   res.status(200).send("Notificación recibida");
-}) //http://localhost:3000/api/payments/notification
+}) //http://localhost:3000/api/payments/notification */
+
+
+paymentRouter.post("/notification", webhook); 
 
 export default paymentRouter
