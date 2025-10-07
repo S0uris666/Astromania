d1b6c61a59ae27ecf35bebc4374720601c63d4d6
unknown <cami.rivas.dlb@gmail.com>
2025-10-07 18:20:48 -0300

    mercado pago

--- Backend/src/config/mercadoPago.js
+++ Backend/src/config/mercadoPago.js
@@ -1,8 +1,15 @@
 import dotenv from "dotenv";
 dotenv.config();
-// SDK de Mercado Pago
-import { MercadoPagoConfig } from 'mercadopago';
-// Agrega credenciales
-const client = new MercadoPagoConfig({ accessToken: process.env.TEST_ACCESS_TOKEN });
+
+import { MercadoPagoConfig } from "mercadopago";
+
+if (!process.env.MP_ACCESS_TOKEN) {
+  throw new Error("MP_ACCESS_TOKEN is missing");
+}
+
+const client = new MercadoPagoConfig({
+  accessToken: process.env.MP_ACCESS_TOKEN,
+  options: { timeout: 30000, locale: "es-CL" }
+});
 
 export default client;
