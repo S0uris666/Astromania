dafd87474fd24d67853d35c1654b7d9c568b589e
unknown <cami.rivas.dlb@gmail.com>
2025-10-07 18:27:57 -0300

    se agrega upload de imagenes

--- Backend/src/routes/service.product.route.js
+++ Backend/src/routes/service.product.route.js
@@ -2,9 +2,11 @@ import {Router} from "express";
 import { createServiceProduct, getAllServiceProducts/* , getServiceProductById, updateServiceProduct, deleteServiceProduct */ } from "../controllers/serviceProduct.controller.js";
 import auth from "../middlewares/auth.js";
 import { authRol } from "../middlewares/authRol.js";
+import multer from "multer";
 const serviceProductRouter = Router();
+const upload = multer({storage: multer.memoryStorage()}); 
 
-serviceProductRouter.post("/user/service-product/create", auth, authRol("admin"), createServiceProduct);
+serviceProductRouter.post("/user/service-product/create", auth, authRol("admin"),upload.array("images",6), createServiceProduct);
 serviceProductRouter.get("/service-products/readall", getAllServiceProducts);
 /* serviceProductRouter.get("/service-product/read/:id", getServiceProductById);
 serviceProductRouter.put("/user/service-product/update/:id", auth, authRol("admin"), updateServiceProduct);
