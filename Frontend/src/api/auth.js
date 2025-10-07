289549ace1673e82817390ba755aad8e18fd182c
unknown <cami.rivas.dlb@gmail.com>
2025-10-07 18:57:55 -0300

    se agregan rutas de productos y eventos

--- Frontend/src/api/auth.js
+++ Frontend/src/api/auth.js
@@ -1,18 +1,23 @@
-import { config } from "@fullcalendar/core/internal";
 import axios from "axios";
 const API = import.meta.env.VITE_BACKEND_URL;
+import client from "./client.js";
 
 //Usuarios
-export const registerRequest = (user) => axios.post(`${API}/register`,user,config)
-export const loginRequest = (user) => axios.post(`${API}/login`,user,config)
-export const verifyRequest = () => axios.get(`${API}/verify-user`,config)
-export const updateRequest = (user) => axios.put(`${API}/update`,user,config)
-export const logoutRequest = () => axios.post(`${API}/logout`,config)
+export const registerRequest = (user) => axios.post("/register",user)
+export const loginRequest = (user) => client.post("/login",user,{})
+export const verifyRequest = () => client.get("/verify-user",{})
+export const updateRequest = (user) => client.put("/update",user,{})
+export const logoutRequest = () => client.post("/logout",{})
 
 //Productos y servicios
-
 export const getServiceProducts= (products) => axios.get(`${API}/service-products/readall`,products)
 
 //eventos
-
 export const getEvents = (events) => axios.get(`${API}/events/readall`, events);
+export const createEvent= (event) => client.post(`/user/event/create`, event);
+
+//Admin 
+export const createServiceProductRequest = (formData) =>
+  client.post("/user/service-product/create", formData, {
+    headers: { "Content-Type": "multipart/form-data" },
+  });
