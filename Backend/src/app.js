cc9fc17b2ad341f02c67bd6bb7ad74053b693673
unknown <cami.rivas.dlb@gmail.com>
2025-10-13 18:18:03 -0300

    se agrega otro origin en cors

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -16,7 +16,7 @@ import routerCloudinary from "./routes/image.route.js";
 
 // Middlewares
 app.use(cors({
-  origin:["http://localhost:5173", "http://localhost:3000"],
+  origin:["http://localhost:5174","http://localhost:5173", "http://localhost:3000"],
   credentials: true,
 }));
 app.use(express.json({ limit: "10mb" }));
