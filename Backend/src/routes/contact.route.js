3340ba41a93b2f37feed926fc5284794d4f1ad7b
unknown <cami.rivas.dlb@gmail.com>
2025-10-23 15:24:06 -0300

    push

--- Backend/src/routes/contact.route.js
+++ Backend/src/routes/contact.route.js
@@ -2,7 +2,7 @@ import express from "express";
 const  contactRouter = express.Router();
 import { createContact } from "../controllers/contact.controller.js";
 
-/* Agregar algun tipo de seguridad para no permitir que cualquiera pueda enviar mensajes de spam */
+
 
 contactRouter.post('/contact', createContact)
 
